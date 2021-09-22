0f90c10c573d687244b608f5ad2298c7f5e98a36
Sara Bonati <sarabonati96@gmail.com>
2021-09-22 12:30:44 +0200

    Try plotly interactive plots

--- apps/ml_basics.py
+++ apps/ml_basics.py
@@ -13,5 +13,18 @@ def app():
     st.title('ML 101')
     
     c1 = Classifier('first')
+    
+    col11, col12= st.columns(2)
+    with col11:
+        x1 = st.number_input('$x_{1}$', min_value=-5, max_value=5, value=1)
+    with col12:
+        y1 = st.number_input('$y_{1}$', min_value=-5, max_value=5, value=1)
+    
+    col21, col22= st.columns(2)
+    with col21:
+        x2 = st.number_input('$x_{2}$', min_value=-5, max_value=5, value=1)
+    with col22:
+        y2 = st.number_input('$y_{2}$', min_value=-5, max_value=5, value=1)
 
-    st.pyplot(c1.generate_data())
+    #st.pyplot(c1.generate_data(x1,y1,x2,y2))
+    st.plotly_chart(c1.generate_data(x1,y1,x2,y2))
--- apps/utilities.py
+++ apps/utilities.py
@@ -8,6 +8,7 @@ import numpy as np
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import plotly.express as px
 import os
 import pandas as pd
 
@@ -18,23 +19,26 @@ class Classifier:
     def __init__(self,name):
         self.name = name 
     
-    def generate_data(self):
-        self.rv1 = multivariate_normal([0.5, -0.2], [[2.0, 0.3], [0.3, 0.5]])
-        self.rv2 = multivariate_normal([1, 2], [[2.0, 0.3], [0.3, 0.5]])
-        self.rv3 = multivariate_normal([-0.5, +0.5], [[2.0, 0.3], [0.3, 0.5]])
+    def generate_data(self,x1,y1,x2,y2):
+        self.rv1 = multivariate_normal([x1,y1], [[2.0, 0.3], [0.3, 0.5]])
+        self.rv2 = multivariate_normal([x2,y2], [[3.0, 0.5], [0.5, 0.1]])
+        #self.rv3 = multivariate_normal([-0.5, +0.5], [[2.0, 0.3], [0.3, 0.5]])
 
         self.samples1 = self.rv1.rvs(size=10)
         self.samples2 = self.rv2.rvs(size=10)
-        self.samples3 = self.rv3.rvs(size=10)
+        #self.samples3 = self.rv3.rvs(size=10)
 
-        fig = plt.figure()
-        gs = gridspec.GridSpec(1,1)
-        ax = {}
+        #fig = plt.figure()
+        #gs = gridspec.GridSpec(1,1)
+        #ax = {}
 
-        ax[0] = fig.add_subplot(gs[0,0])
-        ax[0].scatter(self.samples1[:,0],self.samples1[:,1],s=3,c='b')
-        ax[0].scatter(self.samples2[:,0],self.samples2[:,1],s=3,c='r')
-        ax[0].scatter(self.samples3[:,0],self.samples3[:,1],s=3,c='g')
+        #ax[0] = fig.add_subplot(gs[0,0])
+        fig = px.scatter(x=self.samples1[:,0],y=self.samples1[:,1],color_discrete_sequence=['blue'])
+        fig = px.scatter(x=self.samples2[:,0],y=self.samples2[:,1],color_discrete_sequence=['red'])
+        fig.update_traces(marker_size=10)
+        fig.update_yaxes(range = [-4,4])
+        fig.update_xaxes(range = [-4,4])
+        #ax[0].scatter(self.samples3[:,0],self.samples3[:,1],s=3,c='g')
 
         return fig
     
